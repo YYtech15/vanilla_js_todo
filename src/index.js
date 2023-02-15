907f591abc6ecd7816c4128fa092fb68a17569d6
YYtech15 <midori1140flower@gmail.com>
2023-02-15 16:24:54 +0900

    タスクの削除機能の実装

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const addTask = () => {
     const inputText = document.getElementById("add_text").value;
     document.getElementById("add_text").value = ""; //初期化
-    
+
     const div = document.createElement("div");
     div.className = "list_row";
 
@@ -9,14 +9,25 @@ const addTask = () => {
     li.innerText = inputText;
 
     const button_complete = document.createElement("button");
+    button_complete.addEventListener("click", () => completeTask());
     const button_delete = document.createElement("button");
-    button_complete.innerText="完了";
-    button_delete.innerText="削除";
+    button_delete.addEventListener("click", () => deleteTask(button_delete.parentNode));
+
+    button_complete.innerText = "完了";
+    button_delete.innerText = "削除";
 
     div.appendChild(li);
     div.appendChild(button_complete);
     div.appendChild(button_delete);
     document.getElementById("incomplete_list").appendChild(div);
+};
+
+const completeTask = () => {
+
+};
+
+const deleteTask = (deleteTarget) => {
+    document.getElementById("incomplete_list").removeChild(deleteTarget);
 };
 
 document
