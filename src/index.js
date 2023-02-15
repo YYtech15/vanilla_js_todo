78da47c07770d047bf2016d7487e6685e961f7b4
YYtech15 <midori1140flower@gmail.com>
2023-02-15 16:13:46 +0900

    タスクのDOMの追加生成

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import "./style.css";
+const addTask = () => {
+    const inputText = document.getElementById("add_text").value;
+    document.getElementById("add_text").value = ""; //初期化
+    
+    const div = document.createElement("div");
+    div.className = "list_row";
 
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    const button_complete = document.createElement("button");
+    const button_delete = document.createElement("button");
+    button_complete.innerText="完了";
+    button_delete.innerText="削除";
+
+    div.appendChild(li);
+    div.appendChild(button_complete);
+    div.appendChild(button_delete);
+    document.getElementById("incomplete_list").appendChild(div);
+};
+
+document
+    .getElementById("add_button")
+    .addEventListener("click", () => addTask());
