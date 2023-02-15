c6aa8d96164a894e2ddb50a693a38bd3d227b031
YYtech15 <midori1140flower@gmail.com>
2023-02-15 16:52:19 +0900

    タスクのundo機能の実装

--- src/index.js
+++ src/index.js
@@ -30,17 +30,41 @@ const completeTask = (completeTarget) => {
     const li = document.createElement("li");
     li.innerText = li_text;
 
-    const button_back = document.createElement("button");
-    button_back.innerText = "戻す";
+    const button_undo = document.createElement("button");
+    button_undo.innerText = "戻す";
+    button_undo.addEventListener("click",() => undoTask(button_undo.parentNode));
     
     completeTarget.appendChild(li);
-    completeTarget.appendChild(button_back);
+    completeTarget.appendChild(button_undo);
 
     document.getElementById("complete_list").appendChild(completeTarget);
 };
 
 const deleteTask = (deleteTarget) => {
     document.getElementById("incomplete_list").removeChild(deleteTarget);
+};
+
+const undoTask = (undoTarget) => {
+    document.getElementById("complete_list").removeChild(undoTarget);
+
+    const li_text = undoTarget.firstElementChild.innerText;
+    undoTarget.textContent = null;
+
+    const li = document.createElement("li");
+    li.innerText = li_text;
+
+    const button_complete = document.createElement("button");
+    const button_delete = document.createElement("button");
+    button_complete.addEventListener("click", () => completeTask(button_complete.parentNode));
+    button_delete.addEventListener("click", () => deleteTask(button_delete.parentNode));
+
+    button_complete.innerText = "完了";
+    button_delete.innerText = "削除";
+
+    undoTarget.appendChild(li);
+    undoTarget.appendChild(button_complete);
+    undoTarget.appendChild(button_delete);
+    document.getElementById("incomplete_list").appendChild(undoTarget);
 };
 
 document
